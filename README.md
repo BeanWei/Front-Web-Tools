ea8110fe40a9a70505501f7062c2d9a507a65fce
BeanWei <157318439@qq.com>
2018-12-26 01:54:30 +0800

    Vue监听窗口大小从而控制元素显示隐藏

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Front-Web-Tools
 
 # Vue
-- DragDiv - 20181224
+- DragDiv - 20181224 \
     *** 基于Vue实现Div拖动，适用于屏幕分屏显示的场景 ***
-
+- ListemwindowHeight - 20181225 \
+    *** Vue监听窗口大小从而控制元素显示隐藏 ***
 
 
 @MIT
---
+++ Vue/ListthewindowHeight.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="windowtest">
+        <div class="sub"></div>
+        <div class="sub" v-if="show"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            //监听高度
+            theInitHeight: 0,
+            show: true,
+        }
+    },
+    mounted() {
+        this.theInitHeight = parseInt(document.documentElement.clientHeight);
+        const that = this;
+        window.onresize = function windowResize() {
+            var theHeight = parseInt(document.documentElement.clientHeight);
+            if ((that.theInitHeight - theHeight) > that.theInitHeight/16) {
+                that.show = false;
+            } else {
+                that.show = true;
+            }
+        }
+    },
+}
+</script>
+
+<style scoped>
+.sub {
+    width: 100%;
+    height: 50%;
+}
+</style>
